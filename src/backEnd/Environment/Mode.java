1f408f41daea615bf7c001a3bcf2787339e08e04
Derek Hill <dsh38@duke.edu>
2017-03-30 18:06:37 -0400

    added Mode, author, player APIs

--- src/backEnd/Environment/Mode.java
+++ src/backEnd/Environment/Mode.java
@@ -1,9 +1,23 @@
 package backEnd.Environment;
 
-public class Mode {
+public abstract class Mode {
 
 	public Mode() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public abstract void addStruct();
+	
+	public abstract void removeStruct();
+	
+	public abstract void moveStruct();
+	
+	public abstract void changeTile();
+	
+	public abstract void setStructBehavior();
+	
+	public abstract void setGameRule();
+	
+	
 
 }
