ba34d812bbbc84f05defa09d7778ebbc23b54f5e
Miguel Anderson <mra21@duke.edu>
2017-03-30 23:40:18 -0400

    Removed Skeleton class, added MainMenu and game maker which handles all interactions before any game is created.

---
+++ src/frontEnd/Menus/MainMenu.java
@@ -0,0 +1,26 @@
+package frontEnd.Menus;
+
+import backEnd.GameData;
+
+/**
+ * The main menu which allows the use to
+ * 	1) Create a new game (via GameMaker)
+ *  2) Open a predefined Game Template
+ *  3) Load a saved game
+ * @author Miguel Anderson
+ *
+ */
+
+public class MainMenu {
+
+	public GameData getGameData() {
+		
+		// pop up menu
+		
+		// ask user for decision
+		
+		// return gameData from user selections
+		return null;
+	}
+
+}
--- src/frontEnd/Skeleton/Animator.java
+++ src/frontEnd/Skeleton/Animator.java
@@ -1,4 +1,4 @@
-package frontEnd;
+package frontEnd.Skeleton;
 
 /**
  * 
