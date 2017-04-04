f6fa94aec9a4bcebaf4576a09d3c0888c48ac724
Tim Overeem <to41@duke.edu>
2017-04-03 22:48:51 -0400

    cleaned up errrors

---
+++ src/frontEnd/Skeleton/View/SettingsViewImpl.java
@@ -0,0 +1,26 @@
+package frontEnd.Skeleton.View;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * This class represents the screen that the user sees when the settings button is pressed
+ * @author Tim
+ *
+ */
+public class SettingsViewImpl implements SettingsView{
+
+	private VBox myRoot;
+	public SettingsViewImpl() {
+		myRoot = new VBox();
+	}
+	
+	public void launchSettings(){
+		Stage stage = new Stage();
+		Scene scene = new Scene(myRoot);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+}
