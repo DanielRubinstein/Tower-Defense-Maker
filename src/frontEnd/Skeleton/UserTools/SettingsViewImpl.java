1025bfd90be9d199136359bc503f5566d0778214
Tim Overeem <to41@duke.edu>
2017-04-05 01:25:31 -0400

    finally got bindings to work

--- src/frontEnd/Skeleton/UserTools/SettingsViewImpl.java
+++ src/frontEnd/Skeleton/UserTools/SettingsViewImpl.java
@@ -7,11 +7,20 @@ import frontEnd.Menus.MainMenu;
 import frontEnd.Menus.StartMenu;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.util.List;
+
+import javax.swing.event.ChangeEvent;
 
 /**
  * This class represents the screen that the user sees when the settings button is pressed
@@ -22,14 +31,18 @@ public class SettingsViewImpl implements SettingsView{
 
 	private VBox myRoot;
 	private StartMenu myStartMenu;
-	private Mode myMode;
 	private Stage myStage;
 	private Scene myScene;
+	private SettingsBinding myBinding;
+	private ButtonMenu myButtonMenu;
 	
 	
 	public SettingsViewImpl() {
 		myRoot = new VBox();
 		myStartMenu= new MainMenu();
+		myBinding = new SettingsBinding();
+		myButtonMenu = new ButtonMenu();
+		
 	}
 	
 	public void launchSettings(double width, double height){
@@ -48,44 +61,47 @@ public class SettingsViewImpl implements SettingsView{
 
 	}
 
-	/*
-	 * Buttons to add:
-	 * New
-	 * Load
-	 * Save
-	 * Rules
-	 * Author/Player toggle
-	 */
 	private void addButtons(Stage stage){
-		ButtonMenu menu = new ButtonMenu();
-		myStartMenu.makeGameSelectionButtons(menu, stage);		
-		menu.addButton("Save", e -> save());
-		HBox rules = new HBox();
-		Button viewRules = new Button("View Rules");
-		viewRules.setOnAction(e -> viewRules());
-		Button editRules = new Button("Edit Rules");
-		editRules.setOnAction(e -> editRules());
-		BooleanBinding authorMode = Bindings.createBooleanBinding(()-> System.nanoTime() %2 ==1, viewRules.cancelButtonProperty());
-				//myMode.authorModeProperty());
-		editRules.disableProperty().bind(authorMode.not());
-		//editRules.disabledProperty().addListener(new ChangeListener());
-		rules.getChildren().addAll(viewRules,editRules);
-		menu.addNodeButton(rules);
+		myStartMenu.makeGameSelectionButtons(myButtonMenu, stage);		
+		myButtonMenu.addButton("Save", e -> save());
 		
-		//adding player/godmode switch
-		Button togglePlayer = new Button("Player");
-		togglePlayer.setOnAction(e->togglePlayerMode());
-		Button toggleAuthor = new Button("Author");
-		toggleAuthor.setOnAction(e->toggleAuthorMode());
-		HBox modeButton = new HBox();
-		modeButton.getChildren().addAll(togglePlayer,toggleAuthor);
-		menu.addNodeButton(modeButton);
+		addSideBySideButtons("View Rules",e -> viewRules(),"Edit Rules",e->editRules(),null,Bindings.or(myBinding.valueProperty(), myBinding.valueProperty()).not());
+		addSideBySideButtons("Player",e->togglePlayerMode(),"Author",e->toggleAuthorMode(),null,null);
 		
-		menu.create(myScene.getWidth(), myScene.getHeight());
-		stage.setScene(menu.getScene());
+		
+		myButtonMenu.create(myScene.getWidth(), myScene.getHeight());
+		stage.setScene(myButtonMenu.getScene());
 		stage.show();
-	}
+	} 
 	
+	/**
+	 * Adds two side by side buttons to the button menu. If a button is meant to always be enabled, simply pass null as b1/b2
+	 * @param text1
+	 * @param event1
+	 * @param text2
+	 * @param event2
+	 * @param b1
+	 * @param b2
+	 */
+	private void addSideBySideButtons(String text1, EventHandler<ActionEvent> event1, 
+			String text2, EventHandler<ActionEvent> event2, ObservableValue<? extends Boolean> b1, ObservableValue<? extends Boolean> b2){
+	
+		HBox bothButtons = new HBox();
+		Button button1 = new Button(text1);
+		button1.setOnAction(event1);
+		Button button2 = new Button(text2);
+		button2.setOnAction(event2);
+		
+		if(b1!=null)
+			button1.disableProperty().bind(b1);
+		
+		if(b2!=null)
+			button2.disableProperty().bind(b2);
+		
+		bothButtons.getChildren().addAll(button1,button2);
+		myButtonMenu.addNodeButton(bothButtons);
+			
+	}
 	
 	
 	private void save(){
@@ -93,9 +109,13 @@ public class SettingsViewImpl implements SettingsView{
 	}
 	private void viewRules(){
 		System.out.println("viewing rules in SettingsViewImpl");
+		myBinding.addOne();
+
 	}
 	private void editRules(){
 		System.out.println("editing rules in SettingsViewImpl");
+		myBinding.addOne();
+		System.out.println(myBinding.valueProperty());
 	}
 	private void togglePlayerMode(){
 		System.out.print("toggling player mode");
@@ -103,14 +123,5 @@ public class SettingsViewImpl implements SettingsView{
 	private void toggleAuthorMode(){
 		System.out.println("toggling author mode");
 	}
-	private void isPlayerMode(){
-			
-		
-		
-	}
-	private boolean authorMode(){
-		return false;
-	}
-
 
 }
