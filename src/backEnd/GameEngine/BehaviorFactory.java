cb92b67e0f31f1d8d3469135ebec769c285d7ed1
Daniel Rubinstein <dr148@duke.edu>
2017-04-04 23:56:19 -0400

    oops

--- src/backEnd/GameEngine/BehaviorFactory.java
+++ src/backEnd/GameEngine/BehaviorFactory.java
@@ -1,5 +1,18 @@
 package backEnd.GameEngine;
 
+import com.sun.javafx.geom.Point2D;
+
 public class BehaviorFactory {
+	public Behavior getBehavior(String key){
+		switch (key) {
+		case "MoveBehavior":
+			return new MoveBehavior();
+		case "CollisionBehavior":
+			return new CollisionBehavior();
+		case "DeathBehavior":
+			return new DeathBehavior();
+		default: throw new IllegalArgumentException(); //FIX THIS	
+		}
+	}
 
 }
