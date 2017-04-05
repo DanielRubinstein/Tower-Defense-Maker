655157941382d73c28138f5a171cad4282a8745d
Miguel Anderson <mra21@duke.edu>
2017-04-05 02:32:37 -0400

    Made menu pretty and ready for future changes

--- src/frontEnd/Menus/ButtonMenu.java
+++ src/frontEnd/Menus/ButtonMenu.java
@@ -1,42 +1,59 @@
 package frontEnd.Menus;
 
+import com.sun.prism.paint.Color;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
 public class ButtonMenu {
-	private VBox myRoot;
+	private GridPane myGrid;
+	private VBox myButtonRoot;
 	private Scene myScene;
+	private Label titleLbl;
 	
 	public ButtonMenu(){
-		myRoot = new VBox();
-		
+		initializeGrid();
+		myButtonRoot = new VBox();
 	}
 	
 
+	private void initializeGrid() {
+		myGrid = new GridPane();
+	    myGrid.setHgap(10);
+	    myGrid.setVgap(10);
+	    myGrid.setPadding(new Insets(10));
+	}
+
+
 	public void setText(String text){
-		Text title = new Text(text);
-		title.setTextAlignment(TextAlignment.CENTER);
-		//title.setFont(font);
-		myRoot.getChildren().add(title);
+		titleLbl = new Label(text);
+		titleLbl.setFont(Font.font(32));
+		titleLbl.setUnderline(true);
+		//titleLbl.setStyle("");
 	}
 	
 	public void addSimpleButton(String text, EventHandler<ActionEvent> event){
    	 	Button myButton = new Button(text);
 		myButton.setOnAction(event);
-		myRoot.getChildren().add(myButton);
+		myButtonRoot.getChildren().add(myButton);
 	}
 	
 	public void addButton(Button newButton){
-		myRoot.getChildren().add(newButton);
+		myButtonRoot.getChildren().add(newButton);
 	}
 	
 	public void addButtonRow(String rowTitle, Button... buttons){
@@ -45,11 +62,11 @@ public class ButtonMenu {
 		box.getChildren().add(rowLabel);
 		box.getChildren().addAll(buttons);
 		box.setAlignment(Pos.CENTER);
-		myRoot.getChildren().add(box);
+		myButtonRoot.getChildren().add(box);
 	}
 	
 	public void addNode(Node n){
-		myRoot.getChildren().add(n);
+		myButtonRoot.getChildren().add(n);
 	}
 	
 	public void addBackButton(EventHandler<ActionEvent> event) {
@@ -57,17 +74,12 @@ public class ButtonMenu {
 		
 	}
 	
-	public void create(Double size1, Double size2) {
-   	 	myRoot.setAlignment(Pos.CENTER);
-   	 	myScene = new Scene(myRoot, size1, size2);
-   	 	setSpacing(size1);
 
-	}
 	private void setSpacing(Double size1){
 		
-		double spacing = myRoot.getChildren().size()==0 ? 50 : size1 / (myRoot.getChildren().size()*2);
+		double spacing = myButtonRoot.getChildren().size()==0 ? 50 : size1 / (myButtonRoot.getChildren().size()*2);
 		
-		myRoot.setSpacing(spacing);
+		myButtonRoot.setSpacing(spacing);
 	}
 	
 	
@@ -77,8 +89,15 @@ public class ButtonMenu {
 
 
 	public void create() {
-		myRoot.setAlignment(Pos.CENTER);
-   	 	myScene = new Scene(myRoot);
+		
+		setSpacing(200d);
+		
+		myGrid.add(titleLbl, 0, 0, 2, 1);
+		myGrid.add(myButtonRoot, 0, 1);
+		myGrid.add(new Label("Hopefully will be a description\nof the button moused over"), 1, 1);
+		myGrid.add(new Label("Here the bottom will serve as something, not sure what yet"), 0, 2, 2, 1);
+		//myButtonRoot.setAlignment(Pos.CENTER);
+   	 	myScene = new Scene(myGrid);
 	}
 
 
