b4aaae430656e0dafebf986e7d93534b585792f0
Juan Philippe <jap87@duke.edu>
2017-04-08 13:42:46 -0400

    connected everything, added bankcontroller implementation

--- src/main/ControllerImpl.java
+++ src/main/ControllerImpl.java
@@ -1,31 +1,37 @@
 package main;
 
-import backEnd.Bank.Environment;
-import backEnd.Bank.EnvironmentInterface;
+import backEnd.Bank.BankController;
+import backEnd.Data.DataController;
 import backEnd.GameData.GameData;
 import backEnd.GameData.State.State;
 import backEnd.GameData.State.StateImpl;
 import backEnd.GameEngine.GameProcessController;
-
+import backEnd.Mode.Mode;
+import backEnd.Mode.UserModeType;
 import frontEnd.ViewImpl;
 import frontEnd.Skeleton.Skeleton;
 import frontEnd.Skeleton.SkeletonImpl;
 import javafx.stage.Stage;
 
-public class ControllerImpl implements Controller{
+public class ControllerImpl implements Controller
+{
 	private ViewImpl myView;
-	private EnvironmentInterface myEnvironment;
-	private State myState;
+	private BankController myBankController;
+	private GameData myGameData;
 	private GameProcessController myEngineController;
+	private Mode myMode;
+	private DataController myDataController;
 	
 	public void start(Stage stage){
 		
-
-		SkeletonImpl skeleton = new SkeletonImpl(new ViewImpl());
+		myView = new ViewImpl();
+		myMode = new Mode(null, UserModeType.AUTHOR);
+		SkeletonImpl skeleton = new SkeletonImpl(myView);
 		skeleton.display(stage);
+		myDataController = new DataController();
 		
-		// make view
-		
-		// make model
+		//myGameData = myDataController.getGameData();
+		//myBankController = new BankController(myDataController.getComponents(), myDataController.getTiles());
+	
 	}
 }
