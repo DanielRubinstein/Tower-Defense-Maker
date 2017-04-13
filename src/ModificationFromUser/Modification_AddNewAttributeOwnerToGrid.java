6e2c9ed7b7b334526bc55a66f959319f3984f57e
Derek Hill <dsh38@duke.edu>
2017-04-13 18:05:03 -0400

    adsf

--- src/ModificationFromUser/Modification_AddNewAttributeOwnerToGrid.java
+++ src/ModificationFromUser/Modification_AddNewAttributeOwnerToGrid.java
@@ -11,52 +11,39 @@ import backEnd.GameData.State.Tile;
 import backEnd.Mode.ModeException;
 import javafx.geometry.Point2D;
 
-public class Modification_AddAttributeOwner implements ModificationFromUser {
+/**
+ * Used for adding a preset attribute owner to the grid. Preset attribute owner is passed as parameter in constructor
+ * 
+ * @author Derek
+ *
+ */
+public class Modification_AddNewAttributeOwnerToGrid implements ModificationFromUser {
 	private AttributeOwner newAttrOwn;
 	private Point2D location;
-	private XStream xStream;
 	public static final String DESCRIPTION_TILE = "Replace Tile";
 	public static final String DESCRIPTION_COMPONENT = "Add Component";	
 	public static final String DESCRIPTION_ERROR = "Not a recognized Attribute Owner";	
 	
 
-	public Modification_AddAttributeOwner(AttributeOwner attrOwn, Point2D location) {
-		this.newAttrOwn = attrOwn;
-		this.location = location;
-		xStream = new XStream(new DomDriver());
-		xStream.alias("Component", Component.class);
-		xStream.alias("Tile", Tile.class);
+	public Modification_AddNewAttributeOwnerToGrid(AttributeOwner toAdd) {
+		this.newAttrOwn = toAdd;
+		this.location = (Point2D)toAdd.getAttribute("Position");
 
 	}
 
 	@Override
 	public void invoke(ModelImpl myModel) throws Exception {
-		String serializedAO = xStream.toXML(newAttrOwn);
 		if (newAttrOwn instanceof Tile){
 			switch (myModel.getMode().getUserMode()) {
 			case AUTHOR:
-
-				myModel.getState().getTileGrid().setTile((Tile) xStream.fromXML(serializedAO), location);
+				myModel.getState().getTileGrid().setTile((Tile) newAttrOwn, location);
 				break;
-
 			case PLAYER:
 				 throw new ModeException(myModel.getMode(), DESCRIPTION_TILE);
 			}	
 		} else if (newAttrOwn instanceof Component){
-
-			System.out.println(myModel.getState()+ "   "  +newAttrOwn.getAttribute("Position").getValue());
-			newAttrOwn.setAttributeValue("Position", location);
-			System.out.println(myModel.getState().getComponentGraph() + "  "  +newAttrOwn.getAttribute("Position").getValue());
-			System.out.println((Component) xStream.fromXML(serializedAO));
-
-
-			System.out.println(myModel.getState() + "     "   +location + "   " +newAttrOwn.getAttribute("Position").getValue());
-			//myModel.getState().getComponentGraph().addComponentToGrid((Component) xStream.fromXML(serializedAO), location);
-			
 			myModel.getState().getComponentGraph().addComponentToGrid((Component) newAttrOwn, location);
-			
 
-			//newAttrOwn.addAttribute(attrName, newAttr);
 		} else {
 			// can't be reached
 			// FIXME AHHHHH
