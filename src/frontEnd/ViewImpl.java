370bdb20b9e4572f70b079b3c0e6b4a63dbfc9c5
Tim Overeem <to41@duke.edu>
2017-04-13 00:27:51 -0400

    stuff goes to back end now

--- src/frontEnd/ViewImpl.java
+++ src/frontEnd/ViewImpl.java
@@ -55,7 +55,7 @@ public class ViewImpl implements View{
 		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.getKeyFrames().add(frame);
-		animation.play();
+		//animation.play();
 		
 	}
 
--- src/main/ControllerImpl.java
+++ src/main/ControllerImpl.java
@@ -43,6 +43,7 @@ public class ControllerImpl implements Controller {
 						System.out.println("Modification from user sent to back end");
 					} catch (Exception e) {
 						System.out.println("Error in Modification sent");
+						e.printStackTrace();
 						if (myModel == null){
 							System.out.println("   No model created");
 						}
