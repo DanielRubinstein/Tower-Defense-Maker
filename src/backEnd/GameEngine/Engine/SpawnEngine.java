93070dfd332f80002f9d41cfd74531af8daa6536
Alex <Alex@Salas>
2017-04-20 03:13:01 -0400

    Spawn engine framework complete - still need to connect to frontend

--- src/backEnd/GameEngine/Engine/SpawnEngine.java
+++ src/backEnd/GameEngine/Engine/SpawnEngine.java
@@ -3,36 +3,49 @@ package backEnd.GameEngine.Engine;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import backEnd.Attribute.Attribute;
 import backEnd.GameData.State.Component;
 import backEnd.GameData.State.State;
 import backEnd.GameData.State.Tile;
+import backEnd.GameEngine.Engine.Spawning.SpawnData;
+import backEnd.GameEngine.Engine.Spawning.SpawnQueue;
 /**
  * 
  * @author Alex
  * @author Christian
  */
 public class SpawnEngine implements Engine{
+
 	private int spawnPeriod = 100;
 	private final static String RESOURCES_PATH = "resources/SOMERESOURCEFILE"; //Fix
 	private final static ResourceBundle myResources = ResourceBundle.getBundle(RESOURCES_PATH);
 	private String componentType = "TileType";
 	private String spawnerType = "Spawner";
-	private String spawnQueueAttribute = "SpawnQueue";
-	private String spawnFrequenciesAttribute = "SpawnQueue";
+	private String spawnQueueAttributeName = "SpawnQueue";
 	
 	private boolean paused = true;
-	private Long startSpawnTime;
-	private Long lastSpawnTime;
+	private Long startSpawnTimeMillis;
+	
+	
+	// TODO Add logic in pausing the game and starting again... Fucks up the timeline
+	
 	@Override
 	public void gameLoop(State currentState, double stepTime) {
 		//spawnPeriod = currentState.getSpawnPeriod(); Implement this ish
 		if(!paused){
-			startSpawnTime = System.currentTimeMillis();
+			startSpawnTimeMillis = System.currentTimeMillis();
 		}
 		List<Tile> tileList = currentState.getTileGrid().getAllTiles();
 		for(Tile spawnTile : tileList){
-			if(spawnTile.getAttribute(myResources.getString(componentType)).getValue().equals(spawnerType)){
-				
+			if(!spawnTile.getAttribute(myResources.getString(spawnQueueAttributeName)).getValue().equals(null)){
+				Attribute<SpawnQueue> spawnQueueAttribute = ((Attribute<SpawnQueue>)spawnTile.getAttribute(myResources.getString(spawnQueueAttributeName)));
+				SpawnQueue currentSpawnQueue = spawnQueueAttribute.getValue();
+				// Spawning with frequencies
+				for(Component component:currentSpawnQueue.getNextFrequency(startSpawnTimeMillis - System.currentTimeMillis())){
+					spawn(component);
+				}
+				// Spawning directly
+				spawn(currentSpawnQueue.getNextSpawn());
 				/**
 				 * IF SPAWNING THEN
 				 *    POP SPAWN QUEUE
@@ -41,8 +54,10 @@ public class SpawnEngine implements Engine{
 			}
 		}
 	}
-	
+
 	private void spawn(Component component) {
+		if(component == null){return;}
 		//Spawn it lmao
+		// Not sure how to connect this to frontend
 	}
 }
