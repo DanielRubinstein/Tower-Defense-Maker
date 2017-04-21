556c67f6c989d19d2ba0731befdf7c1b27d47c03
Alex <Alex@Salas>
2017-04-21 18:13:43 -0400

    Spawn edits

--- src/backEnd/GameEngine/Engine/SpawnEngine.java
+++ src/backEnd/GameEngine/Engine/SpawnEngine.java
@@ -5,59 +5,72 @@ import java.util.ResourceBundle;
 
 import backEnd.Attribute.Attribute;
 import backEnd.GameData.State.Component;
+import backEnd.GameData.State.ComponentBuilder;
 import backEnd.GameData.State.State;
 import backEnd.GameData.State.Tile;
-import backEnd.GameEngine.Engine.Spawning.SpawnData;
 import backEnd.GameEngine.Engine.Spawning.SpawnQueue;
+
 /**
  * 
  * @author Alex
  * @author Christian
  */
-public class SpawnEngine implements Engine{
+public class SpawnEngine implements Engine {
 
-	private int spawnPeriod = 100;
-	private final static String RESOURCES_PATH = "resources/SOMERESOURCEFILE"; //Fix
+	private final static String RESOURCES_PATH = "resources/SOMERESOURCEFILE"; // Fix
 	private final static ResourceBundle myResources = ResourceBundle.getBundle(RESOURCES_PATH);
-	private String componentType = "TileType";
-	private String spawnerType = "Spawner";
 	private String spawnQueueAttributeName = "SpawnQueue";
-	
-	private boolean paused = true;
+
+	private boolean internalPaused = true;
+	private boolean gamePaused = true;
+	private boolean started = false;
 	private Long startSpawnTimeMillis;
-	
-	
-	// TODO Add logic in pausing the game and starting again... Fucks up the timeline
-	
+	private Long startPauseTimeMillis;
+
+	// TODO Add logic in pausing the game and starting again... Fucks up the
+	// timeline
+
 	@Override
 	public void gameLoop(State currentState, double stepTime) {
-		//spawnPeriod = currentState.getSpawnPeriod(); Implement this ish
-		if(!paused){
+		// gamePaused = currentState.getEnginePauserinolamo;
+
+		if (internalPaused && gamePaused) {
+			return;
+		}
+		if (!internalPaused && gamePaused) { // Someone pressed pause
+			startPauseTimeMillis = System.currentTimeMillis();
+			return;
+		}
+		if (internalPaused && !gamePaused) { // Someone pressed play
+			startSpawnTimeMillis += System.currentTimeMillis() - startPauseTimeMillis;
+		}
+		if (!started) {
+			started = true;
 			startSpawnTimeMillis = System.currentTimeMillis();
 		}
 		List<Tile> tileList = currentState.getTileGrid().getAllTiles();
-		for(Tile spawnTile : tileList){
-			if(!spawnTile.getAttribute(myResources.getString(spawnQueueAttributeName)).getValue().equals(null)){
-				Attribute<SpawnQueue> spawnQueueAttribute = ((Attribute<SpawnQueue>)spawnTile.getAttribute(myResources.getString(spawnQueueAttributeName)));
+		for (Tile spawnTile : tileList) {
+			if (!spawnTile.getAttribute(myResources.getString(spawnQueueAttributeName)).getValue().equals(null)) {
+				Attribute<SpawnQueue> spawnQueueAttribute = (Attribute<SpawnQueue>) spawnTile
+						.getAttribute(myResources.getString(spawnQueueAttributeName));
 				SpawnQueue currentSpawnQueue = spawnQueueAttribute.getValue();
 				// Spawning with frequencies
-				for(Component component:currentSpawnQueue.getNextFrequency(startSpawnTimeMillis - System.currentTimeMillis())){
+				for (Component component : currentSpawnQueue
+						.getNextFrequency(startSpawnTimeMillis - System.currentTimeMillis())) {
 					spawn(component);
 				}
-				// Spawning directly
+				// Spawning directly with spawn queue
 				spawn(currentSpawnQueue.getNextSpawn());
-				/**
-				 * IF SPAWNING THEN
-				 *    POP SPAWN QUEUE
-				 *    SEND TO FRONTEND
-				 */
 			}
 		}
 	}
 
 	private void spawn(Component component) {
-		if(component == null){return;}
-		//Spawn it lmao
-		// Not sure how to connect this to frontend
+		if (component == null) {
+			return;
+		}
+		ComponentBuilder componentBuilder = new ComponentBuilder(component);
+		Component spawnable = componentBuilder.getComponent();
+		//TODO PUSH TO FRONTEND SCREEN
 	}
 }
