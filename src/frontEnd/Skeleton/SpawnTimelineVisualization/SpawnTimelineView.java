ca90f0142ef9d6629569bbdc61f2b553160c0c2c
Tim Overeem <to41@duke.edu>
2017-04-22 17:19:56 -0400

    can add to view now

--- src/frontEnd/Skeleton/SpawnTimelineVisualization/SpawnTimelineView.java
+++ src/frontEnd/Skeleton/SpawnTimelineVisualization/SpawnTimelineView.java
@@ -12,18 +12,26 @@ import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import ModificationFromUser.Spawning.Modification_AddSpawner;
 
 public class SpawnTimelineView implements SkeletonObject{
 	private View myView;
-	private Rectangle dropZone1;
-	private Rectangle dropZone2;
+	private ScrollPane dropZone1;
+	private ScrollPane dropZone2;
 	private GridPane myRoot;
+	private ScrollPane test;
 
 	public SpawnTimelineView(View view, ReadOnlyDoubleProperty readOnlyDoubleProperty) {
 		myView = view;
@@ -32,14 +40,20 @@ public class SpawnTimelineView implements SkeletonObject{
 		myRoot.setPadding(new Insets(20, 20, 20, 20));
 		myRoot.setVgap(20);
 		myRoot.setHgap(20);
+		createDropZones();
+	}
+	
+	private void createDropZones(){
 		dropZone1 = createDropZone("Single Instance Spawns", 0);
 		dropZone1.setOnDragDropped(e -> {
 			String presetName = e.getDragboard().getString();
 			Component presetComponent = (Component) myView.getBankController().getPreset(presetName);
-			SingleFieldPrompt hey = new SingleFieldPrompt(Arrays.asList("1","2"), "3", "4");
+			SingleFieldPrompt hey = new SingleFieldPrompt(Arrays.asList("Add Spawn",""), "Time interval", "4");
 			long value = Long.parseLong(hey.create());
 			myView.sendUserModification(new Modification_AddSpawner("", presetComponent, value, false));
+			addToDropZone(dropZone1,presetComponent,value);
 		});
+		//timelines in a map in game data
 		dropZone2 = createDropZone("Recurring Spawns", 1);
 		dropZone2.setOnDragDropped(e -> {
 			String presetName = e.getDragboard().getString();
@@ -47,24 +61,52 @@ public class SpawnTimelineView implements SkeletonObject{
 			SingleFieldPrompt hey = new SingleFieldPrompt(Arrays.asList("1","2"), "3", "4");
 			long value = Long.parseLong(hey.create());
 			myView.sendUserModification(new Modification_AddSpawner("", presetComponent, value, true));
+			addToDropZone(dropZone2,presetComponent,value);
 		});
 	}
+	private void addToDropZone(ScrollPane dropZone, Component spawn,long value){
+		HBox spawnBox = new HBox();
+		String spawnImagePath = (String) spawn.getAttribute("ImageFile").getValue();
+		ImageView spawnImage =  createImageView(spawnImagePath);
+		
+		Label name = new Label();
+		name.setText(myView.getBankController().getAOName(spawn));
+		Label valueText = new Label(Long.toString(value));
+		spawnBox.getChildren().add(name);
+		spawnBox.getChildren().add(spawnImage);
+		spawnBox.getChildren().add(valueText);
+		
+		VBox child = (VBox) dropZone.getContent();
+		child.getChildren().add(spawnBox);
+		dropZone.setContent(child);
+	}
+	private ImageView createImageView(String imagePath) {
+		Image image = new Image(getClass().getClassLoader().getResourceAsStream(imagePath));
+		ImageView imv = new ImageView();
+		imv.setImage(image);
+		imv.setPreserveRatio(true);
+		imv.setFitHeight(100);
+		return imv;
+	}
 
-	private Rectangle createDropZone(String string, int i) {
+	private ScrollPane createDropZone(String string, int i) {
 		Label title = new Label(string);
 		myRoot.add(title, i, 0);
-		Rectangle dropZone = new Rectangle();
+		ScrollPane scroll = new ScrollPane();
+		scroll.setHbarPolicy(ScrollBarPolicy.NEVER);
+		scroll.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		scroll.setPrefHeight(300);
+		
+		VBox wrapper = new VBox();
 		ColumnConstraints column1 = new ColumnConstraints();
 	    column1.setPercentWidth(50);
 	    myRoot.getColumnConstraints().add(column1);
-	    dropZone.widthProperty().bind(myRoot.widthProperty().divide(3));
-		dropZone.setHeight(300);
-		dropZone.setFill(Color.LIGHTGRAY);
-		dropZone.setArcHeight(25);
-		dropZone.setArcWidth(25);
-		myRoot.add(dropZone, i, 1);
-		dropZone.setOnDragOver(e -> e.acceptTransferModes(TransferMode.ANY));
-		return dropZone;
+	    wrapper.maxWidthProperty().bind(myRoot.widthProperty().divide(3));
+	    wrapper.setPrefHeight(300);
+	    scroll.setContent(wrapper);
+		myRoot.add(scroll, i, 1);		
+		scroll.setOnDragOver(e -> e.acceptTransferModes(TransferMode.ANY));
+		return scroll;
 	}
 
 	@Override
