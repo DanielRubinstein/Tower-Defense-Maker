d1e4f6905f0acc7c220767fcd02292f217390a93
Alex Salas <alex.salas@duke.edu>
2017-04-23 13:29:46 -0400

    Revert "Merge branch 'revert-10b29a44' into 'master'"
    This reverts merge request !338
    
    Former-commit-id: ea9556e4ff492de8349daca3053c8cbdee8f64d9

---
+++ src/ModificationFromUser/Rules/Modification_EditRule.java
@@ -0,0 +1,18 @@
+package ModificationFromUser.Rules;
+
+import ModificationFromUser.ModificationFromUser;
+import backEnd.ModelImpl;
+
+public class Modification_EditRule implements ModificationFromUser{
+
+	public Modification_EditRule() {
+		
+	}
+	
+	@Override
+	public void invoke(ModelImpl myModel) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
