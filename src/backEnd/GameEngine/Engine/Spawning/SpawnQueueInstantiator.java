7562dc17f38d83ffd99bfcc0cd2eb6356ea1aec6
Juan Philippe <jap87@duke.edu>
2017-04-29 12:03:03 -0400

    added instantiator
    Former-commit-id: 616e98e9cef30bcbcccb78a4be608d6116c504e8

--- src/backEnd/GameEngine/Engine/Spawning/SpawnQueueInstantiator.java
+++ src/backEnd/GameEngine/Engine/Spawning/SpawnQueueInstantiator.java
@@ -1,5 +1,44 @@
 package backEnd.GameEngine.Engine.Spawning;
 
-public class SpawnQueueInstantiator {
+import java.util.List;
 
+public class SpawnQueueInstantiator
+{
+	private List<SpawnDataImpl> myFrequencyQueue;
+	private List<SpawnDataImpl> mySpawnQueue;
+	private double myTimeLastQueueSpawned;
+	private int myCurrentSpawn;
+	private double myGameTime;
+	
+	public SpawnQueueInstantiator(List<SpawnDataImpl> myFrequencyQueue, List<SpawnDataImpl> mySpawnQueue, double myTimeLastQueueSpawned, int myCurrentSpawn,
+			double myGameTime)
+	{
+		this.myFrequencyQueue = myFrequencyQueue;
+		this.mySpawnQueue = mySpawnQueue;
+		this.myTimeLastQueueSpawned = myTimeLastQueueSpawned;
+		this.myCurrentSpawn = myCurrentSpawn;
+		this.myGameTime = myGameTime;
+	}
+	
+	public List<SpawnDataImpl> getFrequencyQueue()
+	{
+		return myFrequencyQueue;
+	}
+	public List<SpawnDataImpl> getSpawnQueue()
+	{
+		return mySpawnQueue;
+	}
+	
+	public double getLastSpawnTime()
+	{
+		return myTimeLastQueueSpawned;
+	}
+	public int getCurrentSpawn()
+	{
+		return myCurrentSpawn;
+	}
+	public double getGameTime()
+	{
+		return myGameTime;
+	}
 }
