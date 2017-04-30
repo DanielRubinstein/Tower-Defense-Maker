4d0be50c8d113e1eb7023b0cae5c2a39637ae9a2
Juan Philippe <jap87@duke.edu>
2017-04-30 15:56:21 -0400

    full level functionality
    Former-commit-id: c6da6dfb5d4be7ebdeed53a64c03d7673d0da484

--- src/backEnd/LevelProgression/LevelProgressionControllerImpl.java
+++ src/backEnd/LevelProgression/LevelProgressionControllerImpl.java
@@ -167,6 +167,7 @@ public class LevelProgressionControllerImpl implements LevelProgressionControlle
 			{
 				gamesMap.put(myMode.getGameMode(), new ArrayList<String>());
 			}			
+			
 			saveGamesMap();
 			
 		} catch (XMLReadingException e) {
@@ -182,7 +183,7 @@ public class LevelProgressionControllerImpl implements LevelProgressionControlle
 		{
 			splashScreenLoader.accept(new SplashScreenData("you won the game!", SplashScreenType.GAME_WON, () -> System.exit(0)));
 		}
-		else gameLoader.accept(getNextLevel());
+		else gameLoader.accept(new File("data/games/" + myMode.getGameMode() + "/templates/" + getNextLevel()));
 	}
 
 	@Override
