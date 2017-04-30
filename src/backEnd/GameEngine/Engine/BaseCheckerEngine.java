62296a00698180a18cb790172f95e94398bfc1db
Alex Salas <ags40@duke.edu>
2017-04-30 16:16:54 -0400

    PVZ BB
    Former-commit-id: c0fd927684443fb34d6695131e8bbdc30339c700

--- src/backEnd/GameEngine/Engine/BaseCheckerEngine.java
+++ src/backEnd/GameEngine/Engine/BaseCheckerEngine.java
@@ -6,19 +6,21 @@ import backEnd.GameData.State.ComponentImpl;
 import backEnd.GameData.State.State;
 import backEnd.GameData.State.Tile;
 import javafx.geometry.Point2D;
+import resources.constants.StringResourceBundle;
 
 public class BaseCheckerEngine implements Engine {
 
 	private State myState;
 	private Tile currentTile;
-
+	private StringResourceBundle STRING_RESOURCES = new StringResourceBundle();
+	
 	@Override
 	public void gameLoop(GameData gameData, double stepTime) {
 		myState = gameData.getState();
 		for (Component c : myState.getComponentGraph().getAllComponents()) {
 			Point2D currentLocation = c.<Point2D>getAttribute("Position").getValue();
 			currentTile = gameData.getState().getTileGrid().getTileByScreenPosition(currentLocation);
-			if (currentTile != null && ((Boolean) currentTile.getAttribute("GoalTile").getValue())) {
+			if (currentTile != null && ((boolean) currentTile.getAttribute("GoalTile").getValue()) && c.getAttribute(STRING_RESOURCES.getFromAttributeNames("Type")).getValue().equals(STRING_RESOURCES.getFromValueNames("EnemyType"))) {
 				gameData.getStatus().decrementStatusItem("Health", 1);
 				c.setAttributeValue("Health", 0);
 			}
