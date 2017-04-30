11a1ffba9a569e1fdbdd3ad69d8336aa1e262eea
Tim Overeem <to41@duke.edu>
2017-04-30 19:26:12 -0400

    refactoring
    Former-commit-id: 19913b669d39ada821293102ea2ecc7233f3bf10

--- src/frontEnd/Skeleton/UserTools/OptionsCornerImpl.java
+++ src/frontEnd/Skeleton/UserTools/OptionsCornerImpl.java
@@ -7,6 +7,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import resources.constants.StringResourceBundle;
+import resources.constants.numeric.NumericResourceBundle;
+import resources.constants.numeric.ScreenConstants;
 /**
  * This class represents the right corner of the bottomRoot (and the bottom right corner of the display). This class provides
  * functionality for the buttons the user can select and a ModeIndicator.
@@ -17,18 +20,26 @@ public class OptionsCornerImpl implements OptionsCorner{
 
 	private VBox myRoot;
 	private OptionsSelection userOptions;
+	private ScreenConstants screenResources = new ScreenConstants();
+	private NumericResourceBundle numResources = new NumericResourceBundle();
+	private StringResourceBundle strResources = new StringResourceBundle();
 	
 	public OptionsCornerImpl(View view) {
 		myRoot = new VBox();
+		setDimensions();
 		setUserOptions(view);
 		setModeToggle(view);
-		myRoot.setSpacing(10d);
+		myRoot.setSpacing(numResources.getFromSizing("StandardSpacing"));
+	}
+
+	private void setDimensions() {
+		myRoot.setPrefSize(screenResources.getSideWidth(), screenResources.getBottomHeight());
 	}
 
 	private void setModeToggle(View view) {
-		//adding player/godmode switch
 		Runnable changeMode = () -> view.sendUserModification(new Modification_ChangeMode());
-		ToggleSwitch modeToggle = new ToggleSwitch("Player", "Author", view.getBooleanAuthorModeProperty(), changeMode);
+		ToggleSwitch modeToggle = new ToggleSwitch(strResources.getFromStringConstants("PLAYER"), 
+				strResources.getFromStringConstants("AUTHOR"), view.getBooleanAuthorModeProperty(), changeMode);
 		myRoot.getChildren().add(modeToggle.getRoot());
 	}
 
@@ -40,15 +51,5 @@ public class OptionsCornerImpl implements OptionsCorner{
 		userOptions = new OptionsSelection(view);
 		userOptions.setAlignment(Pos.TOP_LEFT,Priority.ALWAYS);
 		myRoot.getChildren().add(userOptions.getRoot());
-		
 	}
-	@Override
-	public void setSideDimensions(double width, double height) {
-		//myRoot.setPrefSize(width, height);
-		///myRoot.setMaxHeight(height);
-		
-		userOptions.setSize(width, height/4);
-	}
-
-
 }
