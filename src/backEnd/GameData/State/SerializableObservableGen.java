761fce1d3df8b9096d4d75c516a7462ca5805bc8
Tim Overeem <to41@duke.edu>
2017-05-01 02:40:26 -0400

    added comments for observers
    Former-commit-id: d20a8f84e2b899c77ae6f2824d2fa50d079feecc

--- src/backEnd/GameData/State/SerializableObservableGen.java
+++ src/backEnd/GameData/State/SerializableObservableGen.java
@@ -2,14 +2,35 @@ package backEnd.GameData.State;
 
 import java.util.List;
 
+/**
+ * Defines a serializableObservable that takes a generic type. 
+ * @author Tim
+ *
+ * @param <T>
+ */
 public interface SerializableObservableGen<T>{
 
-	void addObserver(SerializableObserverGen<T> o);
+	/**
+	 * Adds this observer to the list of observers.
+	 * @param o
+	 */
+	public void addObserver(SerializableObserverGen<T> o);
 	
-	List<SerializableObserverGen<T>> getObserversGen();
+	/**
+	 * Gets the list of observers for this observable
+	 * @return
+	 */
+	public List<SerializableObserverGen<T>> getObserversGen();
 
-	void clearObservers();
+	/**
+	 * Clears all observers
+	 */
+	public void clearObservers();
 	
-	void setObserversGen(List<SerializableObserverGen<T>> observersave);
+	/**
+	 * Sets the given parameter as an observer
+	 * @param observersave
+	 */
+	public void setObserversGen(List<SerializableObserverGen<T>> observersave);
 
 }
--- src/frontEnd/CustomJavafxNodes/DragDrop/ListDragDropChanger.java
+++ src/frontEnd/CustomJavafxNodes/DragDrop/ListDragDropChanger.java
@@ -1,9 +1,12 @@
 package frontEnd.CustomJavafxNodes.DragDrop;
 
+import javafx.beans.property.BooleanProperty;
+
 public interface ListDragDropChanger<T> {
 
 	/**
 	 * Sets the Boolean ChangedProperty to true
 	 */
-	void setChanged();
+	public void setChanged();
+
 }
