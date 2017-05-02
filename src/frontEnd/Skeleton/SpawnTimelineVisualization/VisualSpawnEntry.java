4b68ce042091e07a8bff48dbb13a503518a6ef6e
Miguel Anderson <mra21@duke.edu>
2017-05-02 13:07:52 -0400

    Work on refactoring spawndata
    Former-commit-id: 27eb4058fd2a3db6d9d095eae0a381c336da27ea

--- src/frontEnd/Skeleton/SpawnTimelineVisualization/VisualSpawnEntry.java
+++ src/frontEnd/Skeleton/SpawnTimelineVisualization/VisualSpawnEntry.java
@@ -1,12 +1,16 @@
 package frontEnd.Skeleton.SpawnTimelineVisualization;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-import ModificationFromUser.Spawning.Modification_EditSpawnDataTime;
+import ModificationFromUser.Spawning.Modification_AddSpawner;
+import ModificationFromUser.Spawning.Modification_EditSpawnData;
 import ModificationFromUser.Spawning.Modification_RemoveSpawner;
 import backEnd.Attribute.AttributeOwnerReader;
 import backEnd.GameEngine.Engine.Spawning.SpawnDataReader;
 import frontEnd.View;
+import frontEnd.CustomJavafxNodes.MultiFieldPrompt;
 import frontEnd.CustomJavafxNodes.SingleFieldPrompt;
 import frontEnd.Skeleton.ScreenGrid.AttributeOwnerVisual;
 import frontEnd.Skeleton.ScreenGrid.AttributeOwnerVisualImpl;
@@ -17,6 +21,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
@@ -24,6 +29,7 @@ public class VisualSpawnEntry implements SkeletonObject {
 	private View myView;
 	private String mySpawnQueueName;
 	private HBox mySpawnBox;
+	private List<Label> myLabels;
 	
 	public VisualSpawnEntry(View view, String spawnQueueName, SpawnDataReader spawnData){
 		myView = view;
@@ -35,7 +41,8 @@ public class VisualSpawnEntry implements SkeletonObject {
 		mySpawnBox = new HBox();
 		
 		String presetName = spawnData.getPresetName();
-		Double time = spawnData.getTime();
+		
+		List<Number> spawnDataNumbers = getSpawnDataNumbers(spawnData);
 		
 		AttributeOwnerReader presetComponent = myView.getBankControllerReader().getPreset(presetName);
 		AttributeOwnerVisual presetVisual = new AttributeOwnerVisualImpl(presetComponent);
@@ -45,18 +52,28 @@ public class VisualSpawnEntry implements SkeletonObject {
 		Label name = new Label();
 		name.setText(presetName);
 		
-		Label valueText = new Label(Double.toString(time));
-		valueText.setOnMouseClicked(e -> {
-			SingleFieldPrompt newPrompt = new SingleFieldPrompt(
-					Arrays.asList("Add Spawn", "Please input a time for your new spawn item"), "Spawn Time Value",
-					"1");
-			Double newValue = newPrompt.getUserInputDouble();
-			if(newValue == null){
-				return;
-			}
-			myView.sendUserModification(new Modification_EditSpawnDataTime(spawnData,newValue));
-			valueText.setText(Double.toString(newValue));
-		});
+		myLabels = new ArrayList<>();
+		
+		for(Number number : spawnDataNumbers){
+			Label valueText = new Label(number.toString());
+			valueText.setOnMouseClicked(e -> {
+				
+				MultiFieldPrompt multiFieldPrompt = new MultiFieldPrompt(3, Arrays.asList("Add Spawn", "Please input values for your new spawn item"),  Arrays.asList("0.0", "0.0", "0"), Arrays.asList("Frequency", "Delay", "Spawns"));
+				
+				List<String> userInput = multiFieldPrompt.create();
+				if(userInput == null){
+					return;
+				}
+				myView.sendUserModification(
+						new Modification_EditSpawnData(spawnData, Double.parseDouble(userInput.get(0)), Double.parseDouble(userInput.get(1)),Integer.parseInt(userInput.get(2))));
+				
+				for(int i = 0 ; i < myLabels.size() ; i++){
+					myLabels.get(i).setText(userInput.get(i));
+				}
+			});
+			myLabels.add(valueText);
+			mySpawnBox.getChildren().add(valueText);
+		}
 		
 		Button remove = new Button("Delete");
 		remove.setOnAction(e -> {
@@ -66,7 +83,7 @@ public class VisualSpawnEntry implements SkeletonObject {
 		Separator separator2 = new Separator();
 		separator2.setOrientation(Orientation.VERTICAL);
 		
-		mySpawnBox.getChildren().add(valueText);
+		
 		mySpawnBox.getChildren().add(separator2);
 		mySpawnBox.getChildren().add(name);
 		mySpawnBox.getChildren().add(spawnImage);
@@ -75,6 +92,14 @@ public class VisualSpawnEntry implements SkeletonObject {
 		mySpawnBox.setSpacing(10);
 	}
 
+
+	private List<Number> getSpawnDataNumbers(SpawnDataReader spawnData) {
+		List<Number> spawnDataNumbers = new ArrayList<>();
+		spawnDataNumbers.add(spawnData.getFrequency());
+		spawnDataNumbers.add(spawnData.getDelay());
+		spawnDataNumbers.add(spawnData.getSpawns());
+		return spawnDataNumbers;
+	}
 
 	@Override
 	public Node getRoot() {
